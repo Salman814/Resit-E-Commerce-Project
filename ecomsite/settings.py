2f6b4b53761bae5c56bbf0a60933031ec2390856
EngrSalman555 <salmankhalil555@gmail.com>
2026-02-07 22:08:54 +0500

    Set ALLOWED_HOSTS and STATIC_ROOT for Render

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
